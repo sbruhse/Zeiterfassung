563c00ed6ea6c7c7e9de1bb3e61430ea90bc980e
Steven Jonscher <steven.jonscher@gmail.com>
2017-06-08 09:19:58 +0200

    added Getter and Setter for Aufgaben

--- Zeiterfassung/src/main/java/backend/Aufgaben.java
+++ Zeiterfassung/src/main/java/backend/Aufgaben.java
@@ -1,7 +1,58 @@
 package backend;
 
+import java.sql.Time;
+import java.util.Date;
+
 /**
  * Created by steven on 08.06.17.
  */
 public class Aufgaben {
+
+    private int taskId;
+    private String taskName;
+    private String taskDescription;
+    private Time taskDeadline;
+
+    public int getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public Time getTaskDeadline() {
+        return taskDeadline;
+    }
+
+    public void setTaskDeadline(Time taskDeadline) {
+        this.taskDeadline = taskDeadline;
+    }
+
+    
+
+    public void createTask() {
+
+
+    }
+
+    public void editTask() {
+
+    }
 }
