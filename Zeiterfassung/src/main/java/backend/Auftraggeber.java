fe9088dc61f7e722783bc3c1b65123ae6a0c8488
Maximilian Sellmer <sellmer@new-communication.de>
2017-06-08 08:59:37 +0200

    added class for auftraggeber

---
+++ Zeiterfassung/src/main/java/backend/Auftraggeber.java
@@ -0,0 +1,21 @@
+package backend;
+
+/**
+ * Created by sellmer on 08.06.17.
+ */
+public class Auftraggeber {
+    String vorname;
+    String nachname;
+    String stadt;
+    String plz;
+    String strasse;
+
+
+    public Auftraggeber() {
+    }
+
+
+    void auftraggeberHinzufuegen() {
+        
+    }
+}
