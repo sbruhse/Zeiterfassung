45f8b72d32ba5b6ada9d6758592268a9bc22b674
Maximilian Sellmer <sellmer@new-communication.de>
2017-06-08 09:42:48 +0200

    added window

--- Zeiterfassung/src/main/java/zeiterfassung/Zeiterfassung.java
+++ Zeiterfassung/src/main/java/zeiterfassung/Zeiterfassung.java
@@ -6,16 +6,35 @@
 package zeiterfassung;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 
 /**
  *
  * @author sbruhse
  */
-public class Zeiterfassung
-{
+public class Zeiterfassung extends Application{
+    Button button;
+
     public static void main(String[] args)
     {
-        System.out.println("Hallo Welt!");
+       launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Windowtitle");
+        button = new Button();
+        button.setText("Click me");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
