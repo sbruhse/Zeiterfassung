b3bdf4260c8480c2d23fb5ce9a2e7026cf65a5a6
Simon Bruhse <sbruhse@sbruhse.de>
2017-06-08 09:20:22 +0200

    Indentitaet groß geschrieben

---
+++ Zeiterfassung/src/main/java/backend/Identitaet.java
@@ -0,0 +1,18 @@
+package backend;
+
+/**
+ * Created by sellmer on 08.06.17.
+ */
+public class Identitaet {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
