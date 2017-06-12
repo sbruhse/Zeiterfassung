27f636aa0fd582fc2fad127b5f8298aa22e17891
Steven Jonscher <steven.jonscher@gmail.com>
2017-06-12 10:40:47 +0200

    Aufgaben json mit gson generieren

--- Zeiterfassung/src/main/java/backend/Aufgaben.java
+++ Zeiterfassung/src/main/java/backend/Aufgaben.java
@@ -1,67 +1,85 @@
 package backend;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Time;
 import java.util.Date;
 
 /**
  * Created by steven on 08.06.17.
  */
-public class Aufgaben
-{
+public class Aufgaben {
 
-	private int taskId;
-	private String taskName;
-	private String taskDescription;
-	private Time taskDeadline;
+    //Deklaration
+    private int taskId;
+    private String taskName;
+    private String taskDescription;
+    private Date taskDeadline;
+
+    //Gson initialisierung
+    Gson gson = new GsonBuilder().create();
 
 
-	public int getTaskId()
-	{
-		return taskId;
-	}
+    public int getTaskId() {
+        return taskId;
+    }
 
-	public void setTaskId(int taskId)
-	{
-		this.taskId = taskId;
-	}
+    public void setTaskId(int taskId) {
+        this.taskId = taskId;
+    }
 
-	public String getTaskName()
-	{
-		return taskName;
-	}
+    public String getTaskName() {
+        return taskName;
+    }
 
-	public void setTaskName(String taskName)
-	{
-		this.taskName = taskName;
-	}
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
 
-	public String getTaskDescription()
-	{
-		return taskDescription;
-	}
+    public String getTaskDescription() {
+        return taskDescription;
+    }
 
-	public void setTaskDescription(String taskDescription)
-	{
-		this.taskDescription = taskDescription;
-	}
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
 
-	public Time getTaskDeadline()
-	{
-		return taskDeadline;
-	}
+    public Date getTaskDeadline() {
+        return taskDeadline;
+    }
 
-	public void setTaskDeadline(Time taskDeadline)
-	{
-		this.taskDeadline = taskDeadline;
-	}
+    public void setTaskDeadline(Date taskDeadline) {
+        this.taskDeadline = taskDeadline;
+    }
 
-	public void createTask()
-	{
+    public void createTask() {
 
-	}
+        Aufgaben task = new Aufgaben();
 
-	public void editTask()
-	{
+        task.setTaskName("Testname");
+        task.setTaskDescription("blablabla");
+        task.setTaskId(01);
 
-	}
+        task.setTaskDeadline(new Date());
+
+        String json = gson.toJson(task);
+
+        try {
+            FileWriter writer = new FileWriter("aufgaben.json");
+            writer.write(json);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void editTask() {
+
+    }
 }
