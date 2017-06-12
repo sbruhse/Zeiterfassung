edb02c4cdcf6647819efff4555b10ff4b8a08c79
Simon Bruhse <sbruhse@sbruhse.de>
2017-06-12 09:49:23 +0200

    Bereich Javadoc fertig

--- Zeiterfassung/src/main/java/backend/Bereich.java
+++ Zeiterfassung/src/main/java/backend/Bereich.java
@@ -1,31 +1,53 @@
 package backend;
 
 /**
- * Created by sellmer on 08.06.17.
+ * Verwaltung von Bereich
+ *
+ * @author sellmer
  */
 public class Bereich
 {
+	/**
+	 * Name des Bereichs
+	 */
 	private String name;
 
+	/**
+	 * Getter für {@link #name name}
+	 * @return
+	 */
 	public String getName()
 	{
 		return name;
 	}
 
+	/**
+	 * Setter für {@link #name name}
+	 * @param name
+	 */
 	public void setName(String name)
 	{
 		this.name = name;
 	}
 
+	/**
+	 * Standardkonstruktor
+	 */
 	public Bereich()
 	{
 	}
 
+	/**
+	 * Erstellen eines Bereichs
+	 */
 	void bereichErstellen()
 	{
 
 	}
 
+	/**
+	 * Bearbeiten eines Bereichs
+	 */
 	void bereichBearbeiten()
 	{
 
