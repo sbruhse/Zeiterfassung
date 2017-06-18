46cb4c931a4465d612c391fba0da7a0a6ca31693
Simon Bruhse <sbruhse@sbruhse.de>
2017-06-18 18:06:27 +0200

    GSON Tests

--- Zeiterfassung/src/main/java/frontend/Mainwindow.java
+++ Zeiterfassung/src/main/java/frontend/Mainwindow.java
@@ -1,6 +1,10 @@
 package frontend;
 
+import backend.Auftraggeber;
+import backend.Bereich;
 import backend.Identitaet;
+import backend.Projekt;
+import com.google.gson.Gson;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -22,7 +26,23 @@ public class Mainwindow extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        /*
+        Simons Tests START
+         */
+
+        // Erstellt 2 Identitäts Objekte
         System.out.println(Identitaet.getObjectsFromJson("[{name:\"test1\"},{name:\"test2\"}]"));
+
+        // Erstellt 1 Projekt Objekt, macht es zu einem JSON String und anschließend wieder zu einem Projekt Objekt
+        Projekt projekt = new Projekt("Test", new Auftraggeber("hans"),new Bereich("FH"));
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(projekt);
+        System.out.println(jsonString);
+
+
+        /*
+        Simons Tests ENDE
+         */
 
         primaryStage.setTitle("Zeiterfassung");
         button = new Button();
