97b3840dbb6c0c4094ecb1bd0a93303140ca9773
Steven Jonscher <steven.jonscher@gmail.com>
2017-07-14 13:57:25 +0200

    Identität Window

---
+++ Zeiterfassung/src/main/java/frontend/AddIdentitaetWindow.java
@@ -0,0 +1,12 @@
+package frontend;
+
+import javax.swing.*;
+
+/**
+ * Created by steven on 14.07.17.
+ */
+public class AddIdentitaetWindow {
+    private JPanel mainPanelIdentitaet;
+    private JTextField txtFieldIdent;
+    private JButton btnAddIdent;
+}
