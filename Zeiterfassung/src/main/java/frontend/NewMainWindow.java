c8a474d413e4f69d23a3026d28cc366d1c7e3bab
Simon Bruhse <sbruhse@sbruhse.de>
2017-07-17 22:46:43 +0200

    Created NewMainWindow

---
+++ Zeiterfassung/src/main/java/frontend/NewMainWindow.java
@@ -0,0 +1,26 @@
+package frontend;
+
+import javax.swing.*;
+
+/**
+ * Created by sbruhse on 17.07.2017.
+ */
+public class NewMainWindow extends JFrame
+{
+    public JPanel mainPanel;
+    private JButton auftraggeberButton;
+    private JButton identitätenButton;
+    private JPanel leftPanel;
+    private JTree tree1;
+    private JPanel centerPanel;
+    private JButton startButton;
+    private JButton stopButton;
+    private JTextArea textArea1;
+
+    public NewMainWindow()
+    {
+
+    }
+
+
+}
